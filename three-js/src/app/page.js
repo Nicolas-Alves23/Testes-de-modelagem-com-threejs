544edbe89f2240d678f8ccaa3b168a612d72c214
Nicolas-Alves23 <nicolasvilelabarros@gmail.com>
2025-08-24 16:01:07 -0300

    modificacao para js

--- three-js/src/app/page.js
+++ three-js/src/app/page.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { Model as PersonagemMenino } from "../components/Personagem_menino";
+import Model from "../components/Personagem_menino";
 
 export default function Scene() {
   return (
@@ -11,9 +11,7 @@ export default function Scene() {
       <Canvas camera={{ position: [3, 2, 3], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} />
-
-        <PersonagemMenino scale={1} position={[0, 0, 0]} />
-
+        <Model scale={0.8} position={[0, -1.9, 0]} />
         <OrbitControls />
       </Canvas>
     </main>
